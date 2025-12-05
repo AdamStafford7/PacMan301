f493be3c7bb91dcfbf713bfa4f13c11161689bae
Adam Stafford <adamstafford82@gmail.com>
2025-12-05 18:32:07 -0500

    Update GameLauncher.java

--- pacman-main/src/java/game/GameLauncher.java
+++ pacman-main/src/java/game/GameLauncher.java
@@ -1,7 +1,26 @@
+/*
+ * -----------------------------------------------------------------------------------
+ * PROJECT: Pac-Man Program 3
+ * AUTHORS: Adam Stafford, Barnabas Fluck
+ * DATE: 5 December 2025
+ * PURPOSE: Implementation of Pac-Man game with ghosts using various strategies based on research paper exploring MADDPG for UAV swarm control.
+ * -----------------------------------------------------------------------------------
+ * CITATION:
+ * Logic implemented based on the paper:
+ * "EXPLORING THE POSSIBILITIES OF MADDPG FOR UAV SWARM CONTROL BY SIMULATING IN PAC-MAN ENVIRONMENT"
+ * Authors: Artem Novikov, Sergiy Yakovlev, Ivan Gushchin
+ * Published in: Radioelectronic and Computer Systems, 2025, no. 1(113)
+ * DOI: 10.32620/reks.2025.1.21
+ * * Implemented Algorithms:
+ * - [cite_start]Pinky: A* Search [cite: 12, 133]
+ * - [cite_start]Inky: Hybrid Strategy (BFS + Vector) [cite: 13]
+ * -----------------------------------------------------------------------------------
+ */
+
 package game;
 
-import javax.swing.*;
 import java.io.IOException;
+import javax.swing.*;
 
 //Point d'entrée de l'application
 public class GameLauncher {
